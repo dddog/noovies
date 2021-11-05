44ede7267ef183045c9b94738608edfd44fb42de
dddog <azskw1101@gmail.com>
2021-11-05 17:04:18 +0900

    init

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from "expo-app-loading";
+import React, { useState } from "react";
+import * as Font from "expo-font";
+import { Asset } from "expo-asset";
+import { Image, Text } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { NavigationContainer } from "@react-navigation/native";
+import Tabs from "./navigation/Tabs";
+import Stack from "./navigation/Stack";
+
+const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+
+const loadImages = (images) =>
+  images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.loadAsync(image);
+    }
+  });
 
 export default function App() {
+  const [ready, setReady] = useState(false);
+  const onFinish = () => setReady(true);
+  const startLoading = async () => {
+    // 시작에 필요한 자원 로딩하는 곳
+    // ex)await new Promise((resolve) => setTimeout(resolve, 10000));
+    const fonts = loadFonts([Ionicons.font]);
+    const images = loadImages([
+      require("./yanadoo.jpg"),
+      "https://search.naver.com/search.naver?where=nexearch&sm=top_brd&fbm=0&ie=utf8&query=%EC%86%8C%EC%83%81%EA%B3%B5%EC%9D%B8%EC%9D%98+%EB%82%A0",
+    ]);
+    await Promise.all([...fonts, ...images]);
+  };
+  if (!ready) {
+    return (
+      <AppLoading
+        startAsync={startLoading}
+        onFinish={onFinish}
+        onError={console.error}
+      />
+    );
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ navigation/Tabs.js
@@ -0,0 +1,71 @@
+import React from "react";
+
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Movies from "../screens/Movies";
+import Tv from "../screens/Tv";
+import Search from "../screens/Search";
+import { useColorScheme } from "react-native";
+import { BLACK_COLOR, YELLOW_COLOR, DARK_GREY, LIGHT_GREY } from "../colors";
+import { Ionicons } from "@expo/vector-icons";
+
+const Tab = createBottomTabNavigator();
+
+const Tabs = () => {
+  const isDark = useColorScheme() === "dark";
+
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: isDark ? BLACK_COLOR : "white",
+        },
+        tabBarActiveTintColor: isDark ? YELLOW_COLOR : BLACK_COLOR,
+        tabBarInactiveTintColor: isDark ? DARK_GREY : LIGHT_GREY,
+        headerStyle: {
+          backgroundColor: isDark ? BLACK_COLOR : "white",
+        },
+        headerTitleStyle: {
+          color: isDark ? "white" : BLACK_COLOR,
+        },
+        tabBarLabelStyle: {
+          marginTop: -5,
+          fontSize: 13,
+          fontWeight: "700",
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Movies"
+        component={Movies}
+        options={{
+          tabBarIcon: ({ color, size }) => {
+            console.log(color, size);
+            return <Ionicons name="film-outline" size={size} color={color} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="TV"
+        component={Tv}
+        options={{
+          tabBarIcon: ({ color, size }) => {
+            console.log(color, size);
+            return <Ionicons name="tv-outline" size={size} color={color} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({ color, size }) => {
+            console.log(color, size);
+            return <Ionicons name="search-outline" size={size} color={color} />;
+          },
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default Tabs;
