560ae899081852fbec8f4c4f7ff5b10c5de9d378
dddog <azskw1101@gmail.com>
2021-11-16 15:02:44 +0900

    슬라이드 및 포스터 분리

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,15 +1,9 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React, { useEffect, useState } from "react";
-import {
-  ActivityIndicator,
-  Dimensions,
-  StyleSheet,
-  useColorScheme,
-} from "react-native";
+import { ActivityIndicator, Dimensions } from "react-native";
 import Swiper from "react-native-swiper";
 import styled from "styled-components/native";
-import { makeImgPath } from "../utils";
-import { BlurView } from "expo-blur";
+import Slide from "../components/Slide";
 
 const API_KEY = "64e7e0c80d0f8ff24f3087834a8848b7";
 
@@ -25,46 +19,31 @@ const Loader = styled.View`
   align-items: center;
 `;
 
-const BgImg = styled.Image``;
-
-const Poster = styled.Image`
-  width: 100px;
-  height: 160px;
-  border-radius: 5px;
-`;
-
-const Title = styled.Text`
-  font-size: 16px;
-  font-weight: 600;
-  color: white;
-`;
-
-const Wrapper = styled.View`
-  flex-direction: row;
-  height: 100%;
-  justify-content: center;
-  align-items: center;
-`;
-const Column = styled.View`
-  width: 40%;
-  margin-left: 15px;
-`;
-const Overview = styled.Text`
-  margin-top: 10px;
-  color: rgba(255, 255, 255, 0.8);
-`;
-const Votes = styled(Overview)`
-  font-size: 12px;
-`;
-
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = ({
   navigation: { navigate },
 }) => {
-  const isDark = useColorScheme() === "dark";
   const [loading, setLoading] = useState(true);
   const [nowPlaying, setNowPlaying] = useState([]);
+  const [upcoming, setUpcoming] = useState([]);
+  const [trending, setTrending] = useState([]);
+  const getTrending = async () => {
+    const { results } = await (
+      await fetch(
+        `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}`
+      )
+    ).json();
+    setTrending(results);
+  };
+  const getUpcoming = async () => {
+    const { results } = await (
+      await fetch(
+        `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1&region=KR`
+      )
+    ).json();
+    setUpcoming(results);
+  };
   const getNowPlaying = async () => {
     const { results } = await (
       await fetch(
@@ -73,10 +52,13 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = ({
     ).json();
     // console.log(data);
     setNowPlaying(results);
+  };
+  const getData = async () => {
+    await Promise.all([getTrending(), getUpcoming(), getNowPlaying()]);
     setLoading(false);
   };
   useEffect(() => {
-    getNowPlaying();
+    getData();
   }, []);
   return loading ? (
     <Loader>
@@ -93,32 +75,16 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = ({
         showsPagination={false}
         containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
       >
-        {nowPlaying.map((movie) => {
-          return (
-            <View key={movie.id}>
-              <BgImg
-                style={StyleSheet.absoluteFill}
-                source={{ uri: makeImgPath(movie.backdrop_path) }}
-              />
-              <BlurView
-                tint={isDark ? "dark" : "light"}
-                intensity={95}
-                style={StyleSheet.absoluteFill}
-              >
-                <Wrapper>
-                  <Poster source={{ uri: makeImgPath(movie.poster_path) }} />
-                  <Column>
-                    <Title>{movie.original_title}</Title>
-                    <Overview>{movie.overview.slice(0, 90)}...</Overview>
-                    {movie.vote_average > 0 ? (
-                      <Votes>{movie.vote_average}/10</Votes>
-                    ) : null}
-                  </Column>
-                </Wrapper>
-              </BlurView>
-            </View>
-          );
-        })}
+        {nowPlaying.map((movie) => (
+          <Slide
+            key={movie.id}
+            backdropPath={movie.backdrop_path}
+            posterPath={movie.poster_path}
+            originalTitle={movie.original_title}
+            voteAverage={movie.vote_average}
+            overview={movie.overview}
+          />
+        ))}
       </Swiper>
     </Container>
   );
