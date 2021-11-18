36febbd8e202b7f7bcc41401491d09339575ab8b
dddog <azskw1101@gmail.com>
2021-11-18 18:10:44 +0900

    Detail Screen add, 미디어 액션 추가

--- navigation/Stack.js
+++ navigation/Stack.js
@@ -1,32 +1,17 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Text, View, TouchableOpacity } from "react-native";
-
-const Screen1 = ({ navigation: { navigate } }) => (
-  <TouchableOpacity onPress={() => navigate("2")}>
-    <View>
-      <Text>1</Text>
-    </View>
-  </TouchableOpacity>
-);
-const Screen2 = () => (
-  <View>
-    <Text>2</Text>
-  </View>
-);
-const Screen3 = () => (
-  <View>
-    <Text>3</Text>
-  </View>
-);
+import Detail from "../components/Detail";
 
 const NativeStack = createNativeStackNavigator();
 
 const Stack = () => (
-  <NativeStack.Navigator>
-    <NativeStack.Screen name="1" component={Screen1} />
-    <NativeStack.Screen name="2" component={Screen2} />
-    <NativeStack.Screen name="3" component={Screen3} />
+  <NativeStack.Navigator
+    screenOptions={{
+      headerBackTitleVisible: false,
+    }}
+  >
+    <NativeStack.Screen name="Detail" component={Detail} />
   </NativeStack.Navigator>
 );
 
